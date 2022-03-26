db59bf40d82eafe440d777b187f7dd80b6dc8585
Fahreza <fahreza_12@apps.ipb.ac.id>
2022-03-27 03:43:39 +0700

    update PokeCard component

---
+++ src/components/PokeCard.js
@@ -0,0 +1,85 @@
+import React, { useContext } from 'react'
+import styled from '@emotion/styled'
+import { css } from '@emotion/react'
+import {Card, P} from './sharedComponents'
+import { colors } from '../colors'
+import { MyPokemonContext } from '../MyPokemonContext'
+
+function PokeCard({pokemon}) {
+  const {myPokemons, setMyPokemons} = useContext(MyPokemonContext)
+
+  const addPokemon = () => {
+    let nicknameList = []
+    if (myPokemons[pokemon.id]) {
+      console.log("udah ada")
+      nicknameList = [...myPokemons[pokemon.id], "nickame"]
+    } else {
+      console.log("belum ada")
+      nicknameList = ["nickname"]
+    }
+    
+    setMyPokemons(oldPokemons => ({...oldPokemons, [pokemon.id]: nicknameList}))
+    console.log(myPokemons)
+  }
+
+  const HoverCard = styled(Card)`
+    &:hover {
+      cursor: pointer;
+      border: 4px solid ${colors.darkBlue};
+    }
+  `
+  const TextSection = styled.div`
+    display: block;
+    padding: 1.2rem 0.8rem;
+    height: auto;
+    background-color: ${colors.darkBlue};
+    color: white;
+  `
+
+  const Sub = styled(P)`
+    display: inline;
+    font-size: 1.2rem;
+    font-weight: 500;
+    color: ${colors.lightBlue};
+  `
+
+  const Name = styled(P)`
+    font-weight: bold;
+    font-size: 2.1rem;
+  `
+
+  const PokeImage = styled.img`
+    display: block;
+    width: 100%;
+    padding: 3rem 0;
+    margin: auto;
+    height: 10rem;
+    text-align: center;
+    transition: transform 0.4s ease;
+    &:hover {
+      transform: scale(1.1);
+    }
+  `
+
+  const Id = styled(P)`
+    position: absolute;
+    left: 0.5rem;
+    top: 0.5rem;
+    font-weight: bold;
+    font-size: 2rem;
+    color: ${colors.lightBlue};
+  `
+
+  return (
+    <HoverCard>
+      <Id>#{pokemon.id}</Id>
+      <PokeImage src={pokemon.image}/>
+      <TextSection>
+        <Name>{pokemon.name}</Name>
+        <Sub>Owned - {myPokemons[pokemon.id] ? myPokemons[pokemon.id].length : 0}</Sub>
+      </TextSection>
+    </HoverCard>
+  )
+}
+
+export default PokeCard
